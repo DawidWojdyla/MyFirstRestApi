9df0470675e33149832224dca3f383ddc5dcbc6a
Dawid <dawidw23@poczta.onet.pl>
2021-02-03 19:12:56 +0100

    MyDB class for connection with database using PDO

---
+++ MyDB.php
@@ -0,0 +1,30 @@
+<?php
+class MyDB
+{
+	private $dbo = null;
+  
+	function __construct($host, $user, $password, $dbName, $dbType = 'mysql', $charset = 'utf8') {
+		$this -> dbo = $this -> initDB($host, $user, $password, $dbName, $dbType = 'mysql', $charset = 'utf8');
+	}
+  
+	function initDB($host, $user, $password, $dbName, $dbType, $charset) {
+		$dsn = "$dbType:host=$host;dbname=$dbName;charset=$charset";
+		$options = [
+			PDO::ATTR_EMULATE_PREPARES => false,
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+		];
+			
+		try {
+			
+			$dbo = new PDO($dsn, $user, $password, $options);
+			
+		} catch (PDOException $e) {
+			
+			echo "Connection Error: " . $e -> getMessage();
+			
+		}
+		
+		return $dbo;
+	}
+}
+?>
